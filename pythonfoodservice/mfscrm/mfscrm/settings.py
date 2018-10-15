44aa5706088f59828333983ecb536f322a9d4da9
jcramsdell <jramsdell@unomaha.edu>
2018-10-14 22:35:38 -0500

    Added .gitignore file

--- pythonfoodservice/mfscrm/mfscrm/settings.py
+++ pythonfoodservice/mfscrm/mfscrm/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
